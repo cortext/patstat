f90d52417b2189bc87908a417eb15bb8e6fe591e
gnupablo <gnu.pablo@gmail.com>
2017-04-11 11:49:40 +0200

    Modify py script to create a herarchy table and ipc list table

--- nomenclatures/ipc_descriptions/02_abstract_from_ipc.py
+++ nomenclatures/ipc_descriptions/02_abstract_from_ipc.py
@@ -5,17 +5,18 @@ import json
 import sys
 import csv
 import pprint
+import mysql.connector
 
-pp = pprint.PrettyPrinter(indent=4)
 
 # Convert from CPC IPC ref format A63H17/273
-# to IPC symbol format A63H0017273000 (zero padded, 4 digit for main group, 6 digits for group)
+# to IPC symbol format A63H0017273000 (zero padded, 4 digit for main
+# group, 6 digits for group)
 def cpcRefToIpc(s):
     pos_subcode = s.find('/')
     if pos_subcode > 0:
         group = '0000' + s[4:pos_subcode]
         group = group[-4:]
-        subgroup = s[pos_subcode+1:] + '000000'
+        subgroup = s[pos_subcode + 1:] + '000000'
         subgroup = subgroup[0:6]
         return s[0:4] + group + subgroup
     elif len(s) > 4:
@@ -25,7 +26,9 @@ def cpcRefToIpc(s):
     else:
         return s
 
-# Get IPC context and, if exists, return IPC id    
+# Get IPC context and, if exists, return IPC id
+
+
 def CheckIPCContext(s):
     code = cpcRefToIpc(s)
     serviceurl_ipc = 'http://pat-clas.t3as.org/pat-clas-service/rest/v1.0/IPC/ancestorsAndSelf?symbol='
@@ -34,37 +37,57 @@ def CheckIPCContext(s):
     uh = urllib.urlopen(url_ipc)
     data = uh.read()
     js = json.loads(str(data))
-    #pp.pprint(js)
-    retval = ''
+
+    retval = {}
+    full_descr = ' '
 
     if js:
         # getting the level of the code
         for elt in js:
             if elt['symbol'] == code and elt['kind'] != 'n':
                 level_elt = elt['level']
-        
+                retval['code'] = code
+                retval['descr'] = elt['textBody'].split('\n', 1)[0]
+
         for elt in js:
             if elt['level'] <= level_elt and elt['kind'] != 'n':
-                #pp.pprint(elt)
-                retval = retval + ', ' + elt['textBody'].split('\n', 1)[0] # JUST THE FIRST LINE
-                #retval = retval + ', ' + elt['textBody'] # FULL TEXT
-    
-    return retval[1:].strip() # We remove the first delimiter (first char) and the spaces
 
+                if elt['level'] == 0:
+                    retval['parent'] = elt['symbol']
+                if elt['level'] == level_elt - 1:
+                    retval['ancestor'] = elt['symbol']
+
+                full_descr = full_descr + ', ' + \
+                    elt['textBody'].split('\n', 1)[0]  # JUST THE FIRST LINE
+                # full_descr = full_descr + ', ' + elt['textBody'] # FULL TEXT
+    return retval  # We remove the first delimiter (first char) and the spaces
+
+
+pp = pprint.PrettyPrinter(indent=4)
 ifname = 'abstract_from_ipc.input.csv'
-ofname = 'abstract_from_ipc.output.csv'
+cnx = mysql.connector.connect(user='your_user', password='your_password',
+                              host='127.0.0.1',
+                              database='your_database')
+cursor = cnx.cursor()
+add_ipc_hrchy = ("INSERT IGNORE INTO ipc_hierarchy (code, ancestor, parent)"
+                 " VALUES (%s,%s,%s)")
+add_ipc = ("INSERT IGNORE INTO nomen_ipc (code, description)"
+           "VALUES (%s,%s)")
 
 with open(ifname, "rb") as ifile:
     # Reader pipe
     reader = csv.reader(ifile)
-    with open(ofname, "wb") as ofile:
-        # Writer pipe
-        writer = csv.writer(ofile, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        # CSV Headers
-        writer.writerow(['ipc', 'description'])
-        # Loop into input csv to get ipc text and put it in output csv file
-        for row in reader:
-            res = CheckIPCContext(row[0]) 
-            writer.writerow([row[0], res])
 
+    # Loop into input csv to get ipc info and put it in ipc_hierarchy
+    # and nomen_ipc tables
+    for row in reader:
 
+        res = CheckIPCContext(row[0])
+        data_ipc_hrchy = (res['code'], res['ancestor'], res['parent'])
+        data_ipc = (res['code'], res['descr'])
+        cursor.execute(add_ipc, data_ipc)
+        cursor.execute(add_ipc_hrchy, data_ipc_hrchy)
+
+cnx.commit()
+cursor.close()
+cnx.close()
