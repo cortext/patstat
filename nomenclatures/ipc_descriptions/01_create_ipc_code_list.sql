4a120f8bb832690554ff059c8eac27df14625d1f
Lionel Villard <l.villard@esiee.fr>
2017-04-12 14:56:28 +0200

    Add the use of the basic table tls209_appln_ipc

--- nomenclatures/ipc_descriptions/01_create_ipc_code_list.sql
+++ nomenclatures/ipc_descriptions/01_create_ipc_code_list.sql
@@ -1,6 +1,6 @@
 -- ---------------------------------------------------------------------------------
 -- Lionel Villard
--- 2017/04/05
+-- 2017/04/12
 -- Build a list of all IPC code that are inside patstat
 -- ---------------------------------------------------------------------------------
 
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS `a03_01_all_ipc_codes`;
 
 CREATE TABLE a03_01_all_ipc_codes 
 	AS 
-	SELECT ipc_class_symbol, COUNT(appln_id) AS NbApplbn 
-	FROM ipc_technology_frac_ifris
-GROUP BY ipc_class_symbol
+	SELECT ipc_class_symbol, ipc_version, COUNT(appln_id) AS NbApplbn 
+	FROM tls209_appln_ipc
+GROUP BY ipc_class_symbol, ipc_version
 ORDER BY ipc_class_symbol ASC;
