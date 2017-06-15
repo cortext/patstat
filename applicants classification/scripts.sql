e96e385b141c291640db75ab1a81686b5bc723e6
gnupablo <gnu.pablo@gmail.com>
2017-06-15 13:55:03 +0200

    Inventors sequence different to 0

--- applicants classification/scripts.sql
+++ applicants classification/scripts.sql
@@ -8,3 +8,16 @@ SELECT  appln.doc_std_name_id,patstatAvr2014.tls208_doc_std_nms.doc_std_name,app
  WHERE   appln.invt_seq_nr = 0 
  GROUP BY doc_std_name_id;
  
+ 
+-- STEP 2 Inventors sequence different to 0
+
+CREATE TABLE known AS 
+SELECT  appln.doc_std_name_id,patstatAvr2014.tls208_doc_std_nms.doc_std_name,appln.person_id,appln.person_name,appln.invt_seq_nr
+ FROM    patstatAvr2014.applt_addr_ifris as appln
+ INNER JOIN patstatAvr2014.tls208_doc_std_nms ON appln.doc_std_name_id = patstatAvr2014.tls208_doc_std_nms.doc_std_name_id
+ WHERE appln.invt_seq_nr > 0 
+ GROUP BY doc_std_name_id;
+
+-- CLEAN REPEATED DATA
+DELETE FROM known WHERE known.doc_std_name_id in (SELECT  prob_lega.doc_std_name_id FROM prob_lega);
+
