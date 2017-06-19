3cac3693348e7682269f86a485468ccc9cee0039
gnupablo <gnu.pablo@gmail.com>
2017-06-19 15:32:20 +0200

    Modify script and add step 7 graph

--- applicants classification/scripts.sql
+++ applicants classification/scripts.sql
@@ -84,5 +84,39 @@ GROUP BY doc_std_name_id
 HAVING   COUNT(a.doc_std_name_id) < 200
 
 -- CLEAN REPEATED DATA
-DELETE FROM prob_legal WHERE prob_legal in (SELECT known.doc_std_name_id FROM known); 
+DELETE FROM prob_legal WHERE prob_legal in (SELECT prob_person.doc_std_name_id FROM prob_person); 
 
+
+-- STEP 7 Create temporal table
+
+CREATE TABLE temporal AS
+ SELECT
+	   a.person_id,
+	   a.person_name,	 
+	   a.doc_std_name_id,
+	   a.invt_seq_nr,
+	   a.cnt,
+	   b.cnt as cnt2
+	FROM (
+	  SELECT 
+	  person_id,
+	  person_name,	 
+	  doc_std_name_id,
+	  invt_seq_nr,
+	  count(*) as cnt
+	  from patstatAvr2014.applt_addr_ifris
+	  where invt_seq_nr < 1
+	  GROUP BY doc_std_name_id
+	) AS a
+	INNER JOIN (
+	  SELECT 
+	  doc_std_name_id,
+	  count(*) as cnt
+	  from patstatAvr2014.applt_addr_ifris
+	  where invt_seq_nr > 0
+	  GROUP BY doc_std_name_id
+	) AS b ON a.doc_std_name_id = b.doc_std_name_id
+HAVING (a.cnt*100 / (b.cnt + a.cnt)) < 20 
+
+-- CLEAN REPEATED DATA
+DELETE FROM prob_legal WHERE prob_legal.doc_std_name_id in (SELECT doc_std_name_id FROM temporal); 
