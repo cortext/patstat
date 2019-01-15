348c6b539917fb9e99b2e0df6358b3646411a36a
Juan Pablo O <gnu.pablo@gmail.com>
2019-01-15 14:13:00 +0100

    [Fuzzy matching] Add configuration file

--- applicants classification/fuzzy string matching/fuzzy.py
+++ applicants classification/fuzzy string matching/fuzzy.py
@@ -2,24 +2,27 @@ import pymysql
 import pandas as pd
 from sshtunnel import SSHTunnelForwarder
 from os.path import expanduser
+import configparser
 
+config = configparser.ConfigParser()
+config.read('config.ini')
 home = expanduser('~')
 
 # remote database auth
-sql_hostname = ''
-sql_username = ''
-sql_password = ''
-sql_main_database = ''
-sql_port = ''
+sql_hostname = config['remotedb']['db_hostname']
+sql_username = config['remotedb']['db_username']
+sql_password = config['remotedb']['db_password']
+sql_main_database = config['remotedb']['db_database']
+sql_port = config['remotedb']['db_port']
 
 # ssh tunnel auth
-ssh_host = ''
-ssh_user = ''
-ssh_pass = ''
-ssh_port = ''
+ssh_host = config['sshtunnel']['ssh_host']
+ssh_user = config['sshtunnel']['ssh_user']
+ssh_pass = config['sshtunnel']['ssh_pass']
+ssh_port = config['sshtunnel']['ssh_port']
 
-localhost = ''
-forward_port = ''
+localhost = config['localhost']['ip']
+port_forward = config['localhost']['port_forward']
 
 # query variables
 table_applicants = ''
@@ -30,7 +33,7 @@ with SSHTunnelForwarder(
          ssh_username=ssh_user,
          ssh_password=ssh_pass,
          remote_bind_address=(sql_hostname, sql_port),
-         local_bind_address=(localhost, forward_port)) as tunnel:
+         local_bind_address=(localhost, port_forward)) as tunnel:
     conn = pymysql.connect(host=localhost, user=sql_username,
                            passwd=sql_password, db=sql_main_database,
                            port=tunnel.local_bind_port)
