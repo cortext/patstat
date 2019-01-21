56668f1ac89690882f6cc6de47efeeac38cb7297
Juan Pablo O <gnu.pablo@gmail.com>
2019-01-21 11:17:00 +0100

    Insert into probably person when has less than 21 applications

--- applicants classification/scripts.sql
+++ applicants classification/scripts.sql
@@ -39,7 +39,7 @@ SELECT doc_std_name_id,
 FROM entities_recognition_unkown
 WHERE doc_std_name_id IN
     (SELECT doc_std_name_id
-     FROM patstat.invt_addr_ifris
+     FROM invt_addr_ifris
      WHERE SOURCE <> "MISSING");
 
 -- Cleaning repeated data
@@ -68,23 +68,29 @@ WHERE doc_std_name_id IN
     (SELECT doc_std_name_id
      FROM entities_recognition_probably_person);
 
--- STEP 5 From probable legal to probable person set
-INSERT INTO prob_person
-SELECT   
-		 a.person_id,
-		 a.person_name,	 
-		 a.doc_std_name_id,
-		 a.doc_std_name,
-		 a.invt_seq_nr
-FROM     prob_legal as a
-INNER JOIN applt_addr_ifris AS b ON a.doc_std_name_id = b.doc_std_name_id  
-WHERE a.doc_std_name_id IN (SELECT doc_std_name_id FROM invt_addr_ifris)
+-- STEP 5 Insert into probably person when the probably 
+-- legal applicant has not more than 20 applications
+INSERT INTO entities_recognition_probably_person
+SELECT a.doc_std_id,
+       a.doc_std_name,
+       a.person_id,
+       a.person_name,
+       a.invt_seq_nr
+FROM entities_recognition_probably_legal AS a
+INNER JOIN applt_addr_ifris AS b ON a.doc_std_name_id = b.doc_std_name_id
+WHERE a.doc_std_name_id IN
+    (SELECT doc_std_name_id
+     FROM invt_addr_ifris)
 GROUP BY doc_std_name_id
-HAVING   COUNT(a.doc_std_name_id) < 21
-ORDER BY COUNT(a.doc_std_name_id) DESC 
+HAVING COUNT(a.doc_std_name_id) < 21
+ORDER BY COUNT(a.doc_std_name_id) DESC;
 
--- CLEAN REPEATED DATA
-DELETE FROM prob_legal WHERE prob_legal.doc_std_name_id in (SELECT doc_std_name_id FROM prob_person); 
+-- Cleaning data
+DELETE
+FROM entities_recognition_probably_legal
+WHERE doc_std_name_id IN
+    (SELECT doc_std_name_id
+     FROM entities_recognition_probably_person);
 
 -- STEP 6 From probable legal to probable person set2
 INSERT INTO prob_person
