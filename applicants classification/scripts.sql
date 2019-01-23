176734ac47172bf5fb3e7c6d657bb71667672a2a
Juan Pablo O <gnu.pablo@gmail.com>
2019-01-23 09:56:00 +0100

    Create temporal table

--- applicants classification/scripts.sql
+++ applicants classification/scripts.sql
@@ -1,4 +1,4 @@
- USE `your_database`;
+USE `your_database`;
 
 -- STEP 1 Create table with probably legal entities where inventors sequence is equal than 1 
 CREATE TABLE entities_recognition_probably_legal AS
@@ -118,33 +118,43 @@ WHERE doc_std_name_id IN
 
 -- STEP 7 Creating temporal table
 CREATE TABLE temporal AS
- SELECT
-	   a.person_id,
-	   a.person_name,	 
-	   a.doc_std_name_id,
-	   a.invt_seq_nr,
-	   a.cnt,
-	   b.cnt as cnt2
-	FROM (
-	  SELECT 
-	  person_id,
-	  person_name,	 
-	  doc_std_name_id,
-	  invt_seq_nr,
-	  count(*) as cnt
-	  from applt_addr_ifris
-	  where invt_seq_nr < 1
-	  GROUP BY doc_std_name_id
-	) AS a
-	INNER JOIN (
-	  SELECT 
-	  doc_std_name_id,
-	  count(*) as cnt
-	  from applt_addr_ifris
-	  where invt_seq_nr > 0
-	  GROUP BY doc_std_name_id
-	) AS b ON a.doc_std_name_id = b.doc_std_name_id
-HAVING (a.cnt*100 / (b.cnt + a.cnt)) < 20 
+SELECT a.doc_std_name_id,
+       a.person_id,
+       a.person_name,
+       a.invt_seq_nr,
+       a.cnt,
+       b.cnt AS cnt2
+FROM
+  (SELECT doc_std_name_id,
+          person_id,
+          person_name,
+          invt_seq_nr,
+          Count(*) AS cnt
+   FROM applt_addr_ifris
+   WHERE invt_seq_nr < 1
+   GROUP BY doc_std_name_id) AS a
+INNER JOIN
+  (SELECT doc_std_name_id,
+          Count(*) AS cnt
+   FROM applt_addr_ifris
+   WHERE invt_seq_nr > 0
+   GROUP BY doc_std_name_id) AS b ON a.doc_std_name_id = b.doc_std_name_id
+HAVING (a.cnt * 100 / (b.cnt + a.cnt)) < 20;
 
--- CLEAN REPEATED DATA
-DELETE FROM prob_legal WHERE prob_legal.doc_std_name_id in (SELECT doc_std_name_id FROM temporal); 
+INSERT INTO entities_recognition_probably_person
+SELECT a.person_id,
+       a.person_name,
+       a.doc_std_name,
+       a.doc_std_name_id,
+       a.invt_seq_nr
+FROM entities_recognition_probably_legal AS a
+WHERE doc_std_name_id IN
+    (SELECT doc_std_name_id
+     FROM temporal);
+
+-- Cleaning data
+DELETE
+FROM entities_classification_final_3mil_prob_legal
+WHERE doc_std_name_id IN
+    (SELECT doc_std_name_id
+     FROM entities_classification_final_3mil_prob_person);
